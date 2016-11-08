cebb3123ea6ddfb044979a6af05504fb51b9a822
Samielsolh <samielsolh@gmail.com>
2016-11-08 12:57:34 -0500

    Usual

--- Siri.java
+++ Siri.java
@@ -1,15 +1,22 @@
 import java.io.*; //package for system input and output. includes the File class
 import java.util.Scanner;
 
-
 public class Siri{
 
-  public Response(String line)
+  public Response(String line){
+
+    if(line.charAt(0).equals('h') || line.charAt(1).equals('h') ){
+      System.out.println("");
+      System.out.println("");
+
+    }
+
+  }
 
   public static void main( String args[] ) {
     Scanner userPrompt = new Scanner(System.in);
-    System.out.print("State: ");
-    String stateName = userPrompt.nextLine();
+    System.out.print("Hi! My name is Siri 2.0, ask me a question :) (for help enter 'h')  ");
+    String line = userPrompt.nextLine();
 
 
   }
