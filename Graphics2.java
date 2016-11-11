872acc417aadc9fdf969becceac810f9dc1028af
kmarcos3141 <kairedsox@gmail.com>
2016-11-11 12:47:56 -0500

    a

--- Graphics2.java
+++ Graphics2.java
@@ -6,9 +6,15 @@ import javax.swing.JPanel;
 
 public class Graphics extends JPanel
 {
-  public void paintComponent1(Graphics g)
+  private static int signal;
+
+  public Graphics(int i){
+    signal = i;
+  }
+
+  public void paintComponent1(Graphics g, int )
   {
-    if(input.getString("h")){
+    if(signal == 1){
     super.paintComponent(g);
 
     g.setColor(Color.BLUE);
@@ -19,7 +25,7 @@ public class Graphics extends JPanel
 
     g.drawString("Who won the SuperBowl?", 55, 65);
   }
-    else{
+    else if(signal == 2){
     super.paintComponent(g);
 
     g.setColor(Color.BLUE);
@@ -28,19 +34,46 @@ public class Graphics extends JPanel
 
     g.setColor(Color.WHITE);
 
-    g.drawString(general, 55, 65);
+    g.drawString("" 55, 65);
   }
-  String[][] general={
-		{"dsfokfnglksd", "sndjfng", "sogndkgd",
-  "snjdslnf", "sndfkls", "dkfjsdn", "lksdnfsdknf"}
-	};
-  String[][] specific={
-    {""}
- };
+  else if(signal == 3){
+    super.paintComponent(g);
+
+    g.setColor(Color.BLUE);
+
+    g.drawRect(20, 40, 150, 45);
+
+    g.setColor(Color.WHITE);
+
+    g.drawString("" 55, 65);
+  }
+  else if(signal == 4){
+    super.paintComponent(g);
+
+    g.setColor(Color.BLUE);
+
+    g.drawRect(20, 40, 150, 45);
+
+    g.setColor(Color.WHITE);
+
+    g.drawString("" 55, 65);
+  }
+  else if(signal == 5){
+    super.paintComponent(g);
+
+    g.setColor(Color.BLUE);
+
+    g.drawRect(20, 40, 150, 45);
+
+    g.setColor(Color.WHITE);
+
+    g.drawString("" 55, 65);
+  }
   public static void main(String[] args)
   {
     int i = 0;
     while(int i < 20){
+
     JFrame window = new JFrame("Siri");
 
     window.setBounds(300, 300, 200, 150);
